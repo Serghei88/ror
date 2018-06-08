944da08ab0ac1bd7ceab3aba4ad4fcf9a9efa029
Serghei <it.serghei@gmail.com>
2018-06-08 16:59:08 +0300

    code formating

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   include AbstractController::Callbacks
   protect_from_forgery
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-protected
-def configure_permitted_parameters
-   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :remember_me) }
-   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :firstname, :lastname, :email, :password, :remember_me) }
-   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :current_password) }
-end 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :firstname, :lastname, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :current_password) }
+  end
 end
